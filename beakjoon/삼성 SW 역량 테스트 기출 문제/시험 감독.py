105319d430782ada37a0da016185fa522b008144
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-01 16:13:44 +0900

    2023.04.01 (재풀이/코드 간략화)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/시험 감독.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/시험 감독.py
@@ -1,17 +1,12 @@
 import sys
-
-room_num = int(sys.stdin.readline())
-candidators = list(map(int, sys.stdin.readline().split()))
-main, sub = map(int, sys.stdin.readline().split())
-
-total = 0
-for candidator_num in candidators:
-    candidator_num -= main
-    if candidator_num < 0: 
-        candidator_num = 0
-    sub_num, remain = int(candidator_num // sub), candidator_num % sub
-    if remain > 0:
-        sub_num += 1
-    total += (sub_num + 1)
-
-print(total)
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+B, C = map(int, input().split())
+answer = N
+for idx in range(N):
+    num = A[idx] - B
+    if num <= 0:
+        continue
+    answer += (num // C if num % C == 0 else num // C + 1)
+print(answer)
