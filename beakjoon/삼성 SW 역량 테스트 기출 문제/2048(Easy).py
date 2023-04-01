7a5eb8df43cbcaaf1e7b471aa42e1b66bdb1b9d6
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-01 16:07:42 +0900

    2023.04.01 (코드 간략화)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/2048(Easy).py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/2048(Easy).py
@@ -1,103 +1,51 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-# f = open("/Users/hyounjinlee/development/Algorithm-study/beakjoon/삼성 SW 역량 테스트 기출 문제/TC.txt", "w")
-
-N = int(sys.stdin.readline())
-board = []
+N = int(input())
+baord = []
+answer = 0
 for _ in range(N):
-    board.append(list(map(int, sys.stdin.readline().split())))
+    tmp = list(map(int, input().split()))
+    baord.append(tmp)
+    answer = max(answer, max(tmp))
 
-def moving_left_right(board, dir):
-    tmp_board = [tmp.copy() for tmp in board]
-    check = [[False for _ in range(N)] for _ in range(N)]
-    moving = False
-    # print("Direction :", dir)
-    col_range = range(0, N, 1) if dir[1] < 0 else range(N-1, -1, -1)
-    for row in range(N):
-        for col in col_range:
-            next_row = row
-            next_col = col
-            if tmp_board[next_row][next_col] != 0:
-                # print(next_row, next_col)
-                while 0 <= next_row + dir[0] < N and 0 <= next_col + dir[1] < N and tmp_board[next_row + dir[0]][next_col + dir[1]] == 0:
-                    moving = True
-                    # print("Going :", [next_row + dir[0], next_col + dir[1]])
-                    tmp_board[next_row + dir[0]][next_col + dir[1]] = tmp_board[next_row][next_col]
-                    tmp_board[next_row][next_col] = 0
-                    next_row += dir[0]
-                    next_col += dir[1]
-                if 0 <= next_row + dir[0] < N and 0 <= next_col + dir[1] < N and tmp_board[next_row + dir[0]][next_col + dir[1]] == tmp_board[next_row][next_col] and not check[next_row + dir[0]][next_col + dir[1]]:
-                    moving = True
-                    # print("Updating", [next_row + dir[0], next_col + dir[1]])
-                    tmp_board[next_row + dir[0]][next_col + dir[1]] *= 2 
-                    check[next_row + dir[0]][next_col + dir[1]] = True
-                    tmp_board[next_row][next_col] = 0
-                # print("Stepping", tmp_board)
-    return tmp_board, moving
-
-def moving_up_down(board, dir):
-    tmp_board = [tmp.copy() for tmp in board]
-    check = [[False for _ in range(N)] for _ in range(N)]
-    moving = False
-    row_range = range(0, N, 1) if dir[0] <0 else range(N-1, -1, -1)
-    # print("Direction :", dir)
-    for col in range(N):
-        for row in row_range:
-            next_row = row
-            next_col = col
-            if tmp_board[next_row][next_col] != 0:
-                # print(next_row, next_col)
-                while 0 <= next_row + dir[0] < N and 0 <= next_col + dir[1] < N and tmp_board[next_row + dir[0]][next_col + dir[1]] == 0:
-                    moving = True
-                    # print("Going :", [next_row + dir[0], next_col + dir[1]])
-                    tmp_board[next_row + dir[0]][next_col + dir[1]] = tmp_board[next_row][next_col]
-                    tmp_board[next_row][next_col] = 0
-                    next_row += dir[0]
-                    next_col += dir[1]
-                if 0 <= next_row + dir[0] < N and 0 <= next_col + dir[1] < N and tmp_board[next_row + dir[0]][next_col + dir[1]] == tmp_board[next_row][next_col] and not check[next_row + dir[0]][next_col + dir[1]]:
-                    moving = True
-                    # print("Updating", [next_row + dir[0], next_col + dir[1]])
-                    tmp_board[next_row + dir[0]][next_col + dir[1]] *= 2 
-                    check[next_row + dir[0]][next_col + dir[1]] = True
-                    tmp_board[next_row][next_col] = 0
-                # print("Stepping", tmp_board)
-    return tmp_board, moving
-
-def max_board(board):
-    M = 0
-    for row in board:
-        M = max(max(row), M)
-    return M
-
-def solution():
-    que = deque([])
-    que.append([board, 0])
-    direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-    M = 0
-    
-    while que:
-        now_board, count = que.popleft()
-        if count <= 5:
-            M = max(M, max_board(now_board))
-        if count == 5:
-            continue
-        else:
-            for dir in direction:
-                if dir[0] == 0:
-                    next_board, moving_check = moving_left_right(now_board, dir)
+def solution(dir):
+    global answer
+    dirs = {1: [0, 1], 2: [0, -1], 3: [-1, 0], 4: [1, 0]}
+    (drow, dcol) = dirs[dir]
+    next_board = [tmp.copy() for tmp in now_board]
+    addition = [[False for _ in range(N)] for _ in range(N)]
+    for row in range(N - 1 if dir == 4 else 0, -1 if dir == 4 else N, -1 if dir == 4 else 1):
+        for col in range(N - 1 if dir == 1 else 0, -1 if dir == 1 else N, -1 if dir == 1 else 1):
+            now_row, now_col = row, col
+            value = next_board[now_row][now_col]
+            next_board[now_row][now_col] = 0
+            while True:
+                next_row, next_col = now_row + drow, now_col + dcol
+                if not (0 <= next_row < N and 0 <= next_col < N):
+                    next_board[now_row][now_col] = value
+                    break
+                elif next_board[next_row][next_col] == 0:
+                    now_row, now_col = next_row, next_col
+                elif value == next_board[next_row][next_col] and not addition[next_row][next_col]:
+                    next_board[next_row][next_col] += value
+                    answer = max(answer, next_board[next_row][next_col])
+                    addition[next_row][next_col] = True
+                    break
                 else:
-                    next_board, moving_check = moving_up_down(now_board, dir)
-                if moving_check:
-                    que.append([next_board, count + 1])
-                    # f.write(f"========================== {count+1}\n{now_board}\n{next_board}\n")
+                    next_board[now_row][now_col] = value
+                    break
+    return next_board
+
+que = deque([[baord, 0]])
+while que:
+    now_board, now_cnt = que.popleft()
+    if now_cnt == 5:
+        continue
     
-    print(M)
-
-solution()
-
-# tmp = moving_left_right(board, [1,0])[0]
-# print(moving_left_right(board, [1,0])[1])
-# print("/n")
-# for t in tmp:
-#     print(*t)
+    for dir in range(1, 5):
+        next_board = solution(dir)
+        que.append([next_board, now_cnt + 1])
+                
+print(answer)        
