f9a1260b20c56b247cd6d7aba32f36cead4617bd
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-02 19:44:36 +0900

    2023.04.02 (재풀이)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/주사위 굴리기.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/주사위 굴리기.py
@@ -1,40 +1,32 @@
 import sys
+input = sys.stdin.readline
 
-N, M, x, y, K = map(int, sys.stdin.readline().split())
+N, M, x, y, K = map(int, input().split())
 board = []
 for _ in range(N):
-    board.append(list(map(int, sys.stdin.readline().split())))
-dir = {1: [0, 1], 2: [0, -1], 3: [-1, 0], 4: [1, 0]}
-direction = list(map(int, sys.stdin.readline().split()))
-dice = [0 for _ in range(6)]
+    board.append(list(map(int, input().split())))
+movings = list(map(int, input().split()))
+jusawe = {idx : 0 for idx in range(1, 7)}
+dirs = {1: [0, 1], 2: [0, -1], 3: [-1, 0], 4: [1, 0]}
+jusawe[6] = board[x][y]
+board[x][y] = 0
 
-def turn(dice, dir):
-    a, b, c, d, e, f = dice
-    if dir == 1:
-        dice = [d, b, a, f, e, c]
-    elif dir == 2:
-        dice = [c, b, f, a, e, d]
-    elif dir == 3:
-        dice = [e, a, c, d, f, b]
-    elif dir == 4:
-        dice = [b, f, c, d, a, e]
-    return dice
-
-def change_dice():
-    if board[x][y] == 0:
-        board[x][y] = dice[-1]
+for moving in movings:
+    next_x, next_y = x + dirs[moving][0], y + dirs[moving][1]
+    if not (0 <= next_x < N and 0 <= next_y < M):
+        continue
+    x, y = next_x, next_y
+    if moving == 1:
+        jusawe[1], jusawe[3], jusawe[4], jusawe[6] = jusawe[4], jusawe[1], jusawe[6], jusawe[3]
+    elif moving == 2:
+        jusawe[1], jusawe[3], jusawe[4], jusawe[6] = jusawe[3], jusawe[6], jusawe[1], jusawe[4]
+    elif moving == 3:
+        jusawe[1], jusawe[2], jusawe[5], jusawe[6] = jusawe[5], jusawe[1], jusawe[6], jusawe[2]
     else:
-        dice[-1] = board[x][y]
+        jusawe[1], jusawe[2], jusawe[5], jusawe[6] = jusawe[2], jusawe[6], jusawe[1], jusawe[5]
+    if board[x][y] != 0:
+        jusawe[6] = board[x][y]
         board[x][y] = 0
-
-change_dice()
-
-
-for d in direction:
-    next_x = x + dir[d][0]
-    next_y = y + dir[d][1]
-    if 0 <= next_x < N and 0 <= next_y < M:
-        x, y = next_x, next_y
-        dice = turn(dice, d)
-        change_dice()
-        print(dice[0])
+    else:
+        board[x][y] = jusawe[6]
+    print(jusawe[1])
