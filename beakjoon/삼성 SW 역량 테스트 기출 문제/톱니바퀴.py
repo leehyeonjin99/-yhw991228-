a356901a1e20bdf736e761cc119708e807af7760
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-04 22:23:37 +0900

    2023.04.04 (재풀이 / 코드 간략화)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/톱니바퀴.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/톱니바퀴.py
@@ -1,58 +1,43 @@
 import sys
+input = sys.stdin.readline
+circles = []
+for _ in range(4):
+    circles.append(list(map(int, list(input())[:-1])))
+K = int(input())
+rots = []
+for _ in range(K):
+    rots.append(list(map(int, input().split()))) # 1 : 시계 / -1 : 반시계
 
-gear_1 = list(map(int, list(sys.stdin.readline()[:-1])))
-gear_2 = list(map(int, list(sys.stdin.readline()[:-1])))
-gear_3 = list(map(int, list(sys.stdin.readline()[:-1])))
-gear_4 = list(map(int, list(sys.stdin.readline()[:-1])))
-gears = {1: gear_1, 2: gear_2, 3: gear_3, 4: gear_4}
-rotation_count = int(sys.stdin.readline())
-dir = {1: "RIGHT", -1: "LEFT"}
+def rot(c_num, dir):
+    if dir == 1:
+        circles[c_num] = [circles[c_num][-1]] + circles[c_num][:-1]
+    else:
+        circles[c_num] = circles[c_num][1:] + [circles[c_num][0]]
 
-def rotation(gear, dir):
-    tmp_gear = gear.copy()
-    tmp_gear = [tmp_gear[-1]] + tmp_gear[:-1] if dir == 1 else tmp_gear[1:] + [tmp_gear[0]]
-    return tmp_gear
+answer = 0
+for (c_num, dir) in rots:
+    c_num -= 1
+    compare = []
+    for idx in range(3):
+        compare.append(circles[idx][2] == circles[idx + 1][6])
+    rot(c_num, dir)
+    right_dir = dir
+    for right in range(c_num + 1, 4):
+        if not compare[right - 1]:
+            right_dir = -right_dir
+            rot(right, right_dir)
+        else:
+            break
+    left_dir = dir
+    for left in range(c_num - 1, -1, -1):
+        if not compare[left]:
+            left_dir = -left_dir
+            rot(left, left_dir)
+        else:
+            break
 
-def left_check(left_num):
-    # # print(gears[left_num])
-    # # print(gears[left_num - 1])
-    return gears[left_num][6] == gears[left_num - 1][2] if left_num > 1 else True
-
-def right_check(right_num):
-    # # print(gears[right_num])
-    # # print(gears[right_num - 1])
-    return gears[right_num][2] == gears[right_num + 1][6] if right_num < 4 else True
-
-for _ in range(rotation_count):
-    rotation_num, rotation_dir = map(int, sys.stdin.readline().split())
-    now_left_check = left_check(rotation_num)
-    now_right_check = right_check(rotation_num)
-    gears[rotation_num] = rotation(gears[rotation_num], rotation_dir)
-    # print(gears[rotation_num])
-    left_num = right_num = rotation_num
-    left_rotation_dir = right_rotation_dir = rotation_dir
-    # print("LEFT CHECK")
-    while not now_left_check and left_num > 1:
-        left_num -= 1
-        left_rotation_dir = -left_rotation_dir
-        # print(f"{left_num} TURNING {dir[left_rotation_dir]}")
-        now_left_check = left_check(left_num)
-        gears[left_num] = rotation(gears[left_num], left_rotation_dir)
-        # print(gears[left_num])
-        
-    # print("RIGHT CHECK")
-    while not now_right_check and right_num < 4:
-        right_num += 1
-        right_rotation_dir = -right_rotation_dir
-        now_right_check = right_check(right_num)
-        # print(f"{right_num} TURNING {dir[right_rotation_dir]}")
-        gears[right_num] = rotation(gears[right_num], right_rotation_dir)
-        # print(gears[right_num])
-
-score = 0
-for num in range(4):
-    gear_top_num = gears[num + 1][0]
-    if gear_top_num == 1:
-        score += 2**num
-
-print(score)
+answer = 0
+for idx, nums in enumerate(circles):
+    if nums[0] == 1:
+        answer += 1 if idx == 0 else (2 if idx == 1 else (4 if idx == 2 else 8))
+print(answer)
