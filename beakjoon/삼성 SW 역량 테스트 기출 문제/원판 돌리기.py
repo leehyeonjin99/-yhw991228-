c2e90dafa3a1f359fd7f0a4f1b0bbef22f5dcdac
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-07 02:35:16 +0900

    2023.04.06 (재풀이)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/원판 돌리기.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/원판 돌리기.py
@@ -1,101 +1,66 @@
-from collections import deque
 import sys
+from collections import deque
+input = sys.stdin.readline
 
-N, M, T = map(int, sys.stdin.readline().split())
-
-values = []
+N, M, T = map(int, input().split())
+circles = []
 for _ in range(N):
-    values.append(list(map(int, sys.stdin.readline().split())))
+    circles.append(list(map(int, input().split())))
+orders = []
 
-rotations = []
-for _ in range(T):
-    rotations.append(list(map(int, sys.stdin.readline().split())))
-
-dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-for rot_idx, rotation in enumerate(rotations):
-    # print("======================================")
-    # print(f"ROTATOIN {rot_idx} : {rotation}")
-    # 회전
-    x, d, k = rotation
-    for circle_idx in range(1, N+1):
-        if circle_idx % x !=0:
-            continue
-        circle_idx -= 1
-        if d == 0:
-            values[circle_idx] = values[circle_idx][-k:] + values[circle_idx][:-k]
-        else:
-            values[circle_idx] = values[circle_idx][k:] + values[circle_idx][:k]
-    # for value in values:
-    #     print(f"{value}")
-    
-    # 같은 값 찾기
-    cnt = 1
-    group_loc = {}
+drows = [1, -1, 0, 0]
+dcols = [0, 0, 1, -1]
+def search():
     visited = [[0 for _ in range(M)] for _ in range(N)]
-    values_sum = 0
-    values_cnt = 0
+    check = False
+    S, cnt = 0, 0
     for row in range(N):
         for col in range(M):
-            values_sum += values[row][col]
-            values_cnt += 1
-            # 방문한 적 있는 위치
+            if circles[row][col] == 0:
+                continue
+            S += circles[row][col]
+            cnt += 1
             if visited[row][col]:
                 continue
-            # 방문한 적 없지만 값이 지워진 위치
-            elif values[row][col] == 0:
-                values_cnt -= 1
-                continue
-            que = deque([[row, col]])
-            visited[row][col] = cnt
-            group_loc[cnt] = [[row, col]]
-            # print(f"Start {[row, col]}")
+            visited[row][col] = 1
+            que = deque([[row, col, False]])
             while que:
-                now_row, now_col = que.popleft()
-                now_value = values[now_row][now_col]
-                # print("Now Location :", [now_row, now_col])
-                for dir in dirs:
-                    next_row, next_col = now_row + dir[0], now_col + dir[1]
-
-                    # 인접 위치 값 조정
-                    if not 0 <= next_row < N:
+                now_row, now_col, is_set = que.popleft()
+                value = circles[now_row][now_col]
+                if is_set:
+                    circles[now_row][now_col] = 0
+                for (drow, dcol) in zip(drows, dcols):
+                    next_row, next_col = (now_row + drow), (now_col + dcol) % M
+                    if not (0 <= next_row < N):
                         continue
-                    if next_col < 0:
-                        next_col = M -1
-                    elif next_col >= M:
-                        next_col = 0
-                    # 같은 값에 대해서 체크
-                    if not visited[next_row][next_col] and now_value == values[next_row][next_col]:
-                        # print("Next Location :", [next_row, next_col])
-                        que.append([next_row, next_col])
-                        visited[next_row][next_col] = cnt
-                        group_loc[cnt].append([next_row, next_col])
-            cnt += 1
-    # print("주변 GROUPING")
-    # for visit in visited:
-    #     print(f"{visit}")
-
-    change_check = False
-    for cnt, locs in group_loc.items():
-        if len(locs) > 1:
-            change_check = True
-            for (loc_row, loc_col) in locs:
-                values[loc_row][loc_col] = 0
-    # print(f"SUM : {values_sum}, CNT : {values_cnt}")
-    if not change_check:
-        if values_cnt == 0:
-            continue
-        values_mean = values_sum / values_cnt
+                    if not visited[next_row][next_col] and circles[next_row][next_col] == value:
+                        visited[next_row][next_col] = 1
+                        que.append([next_row, next_col, True])
+            if is_set:
+                circles[row][col] = 0
+                check = True
+    if cnt ==  0:
+        return
+    if not check:
+        mean = S / cnt
         for row in range(N):
             for col in range(M):
-                if values[row][col] == 0:
+                if circles[row][col] == 0:
                     continue
-                elif values[row][col] < values_mean:
-                    values[row][col] += 1
-                elif values[row][col] > values_mean:
-                    values[row][col] -= 1
-    # print("평균 혹은 제거")
-    # for value in values:
-    #     print(f"{value}")
+                elif circles[row][col] < mean:
+                    circles[row][col] += 1
+                elif circles[row][col] > mean:
+                    circles[row][col] -= 1
 
-answer = sum(sum(value) for value in values)
-print(answer)
+for _ in range(T):
+    x, d, k = map(int, input().split())
+    for row in range(N):
+        if (row + 1) % x != 0:
+            continue
+        if d == 0:
+            circles[row] = circles[row][-k:] + circles[row][:-k]
+        else:
+            circles[row] = circles[row][k:] + circles[row][:k]
+    search()
+
+print(sum(sum(tmp) for tmp in circles))
--- beakjoon/삼성 SW 역량 테스트 기출 문제/주사위 윷놀이.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/주사위 윷놀이.py
@@ -1,78 +1,48 @@
 import sys
-from itertools import product
+from collections import deque
+orders = list(map(int, sys.stdin.readline().split()))
 
-# f = open("/Users/hyounjinlee/development/Algorithm-study/beakjoon/삼성 SW 역량 테스트 기출 문제/log.txt", mode = "wt")
-
-lengths = list(map(int, sys.stdin.readline().split()))
-# 1: blue, 0: black
-# 칸 정보 : [color, score, next_idxs]
-board = {
-    0: [0, 0, [1]],
-    1: [0, 2, [2]],
-    2: [0, 4, [3]],
-    3: [0, 6, [4]],
-    4: [0, 8, [5]],
-    5: [1, 10, [6, 20]],
-    6: [0, 12,[7]],
-    7: [0, 14, [8]],
-    8: [0, 16, [9]],
-    9: [0, 18, [10]],
-    10: [1, 20, [11, 23]],
-    11: [0, 22, [12]],
-    12: [0, 24, [13]],
-    13: [0, 26, [14]],
-    14: [0, 28, [15]],
-    15: [1, 30, [16, 25]],
-    16: [0, 32, [17]],
-    17: [0, 34, [18]],
-    18: [0, 36, [19]],
-    19: [0, 38, [31]],
-    20: [0, 13, [21]],
-    21: [0, 16, [22]],
-    22: [0, 19, [28]],
-    23: [0, 22, [24]],
-    24: [0, 24, [28]],
-    25: [0, 28, [26]],
-    26: [0, 27, [27]],
-    27: [0, 26, [28]],
-    28: [0, 25, [29]],
-    29: [0, 30, [30]],
-    30: [0, 35, [31]],
-    31: [0, 40, [32]],
-    32: [0, 0, []]
-}
+# 1: 출발 / 32: 도착
+board = {0: [0, 1]}
+for idx in range(1, 20):
+    board[idx] = [idx * 2, idx + 1]
+board[20] = [40, 32]
+board[5].append(21)
+board[10].append(27)
+board[15].append(24)
+board[21] = [13, 22]
+board[22] = [16, 23]
+board[23] = [19, 29]
+board[24] = [28, 25]
+board[25] = [27, 26]
+board[26] = [26, 29]
+board[27] = [22, 28]
+board[28] = [24, 29]
+board[29] = [25, 30]
+board[30] = [30, 31]
+board[31] = [35, 20]
+board[32] = [0]
 
 answer = 0
-for round, horse_comb in enumerate(product(range(4), repeat = 10)):
-    if horse_comb[:4] == (0, 1, 2, 3):
-        break
-    # f.write(f"ROUND {round} : {horse_comb}\n")
-    horse_loc = [0, 0, 0, 0]
-    game_check = True
-    game_answer = 0
-    for horse_idx, moving_length in zip(horse_comb, lengths):
-        # f.write(f"Moving {horse_idx} with {moving_length} : ")
-        if horse_loc[horse_idx] == 32:
-            game_check = False
-            # f.write("현재 종점이므로 움직일 수 없음\n")
-            break
-        # f.write(f"{horse_loc[horse_idx]} => ")
-        for cnt in range(moving_length):
-            if cnt == 0 and board[horse_loc[horse_idx]][0] == 1:
-                horse_loc[horse_idx] = board[horse_loc[horse_idx]][-1][-1]
-                continue
-            horse_loc[horse_idx] = board[horse_loc[horse_idx]][-1][0]
-            if horse_loc[horse_idx] == 32:
+que = deque([[0, 0, [0, 0, 0, 0]]])
+while que:
+    now_idx, now_sum, now_locs = que.popleft()
+    answer = max(answer, now_sum)
+    if now_idx == 10:
+        continue
+    go_cnt = orders[now_idx]
+    for idx in range(4):
+        now_loc = now_locs[idx]
+        if now_loc == 32:
+            continue
+        for cnt in range(go_cnt):
+            now_loc = board[now_loc][-1] if cnt == 0 else board[now_loc][1]
+            if now_loc == 32:
                 break
-        horse_except_loc = horse_loc.copy()
-        horse_except_loc.pop(horse_idx)
-        if horse_loc[horse_idx] != 32 and horse_loc[horse_idx] in horse_except_loc:
-            game_check = False
-            # f.write("이미 자리 있음\n")
-            break
-        game_answer += board[horse_loc[horse_idx]][1]
-        # f.write(f"{horse_loc[horse_idx]} : {board[horse_loc[horse_idx]][1]}점 얻음 즉, {game_answer}점\n")
-    if game_check:
-        answer = max(answer, game_answer)
+        if now_loc < 32 and now_loc in now_locs:
+            continue
+        next_locs = now_locs.copy()
+        next_locs[idx] = now_loc
+        que.append([now_idx + 1, now_sum + board[now_loc][0], next_locs])
 
-print(f"{answer}")
+print(answer)
