e8c3ec9bf33872546f2042c5591cad6253ead3fd
leehyeonjin99 <reviewtest@ajou.ac.kr>
2023-04-07 19:30:51 +0900

    2023.04.07 (재풀이 / 코드 간략화)

--- beakjoon/삼성 SW 역량 테스트 기출 문제/스타트 택시.py
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/스타트 택시.py
@@ -1,116 +1,71 @@
-from collections import deque
 import sys
+from collections import deque
 
-N, M, oil = map(int, sys.stdin.readline().split())
+N, M, taxi_oil = map(int, sys.stdin.readline().split())
 board = []
-
-for row in range(N):
+for _ in range(N):
     board.append(list(map(int, sys.stdin.readline().split())))
+taxi_row, taxi_col = map(int, sys.stdin.readline().split())
+taxi_row, taxi_col = taxi_row - 1, taxi_col - 1
+customers = {}
+for idx in range(M):
+    s_row, s_col, e_row, e_col = map(int, sys.stdin.readline().split())
+    s_row, s_col, e_row, e_col = s_row - 1, s_col - 1, e_row - 1, e_col - 1
+    board[s_row][s_col] = 2
+    customers[(s_row, s_col)] = (e_row, e_col)
 
-taxi = list(map(lambda x : int(x) - 1, sys.stdin.readline().split()))
-
-passenger_list = list(range(1, M + 1))
-passengers_start = {idx : [] for idx in range(1, M + 1)}
-passengers_end = {idx : [] for idx in range(1, M + 1)}
-
-for idx in range(1, M + 1):
-    start_row, start_col, end_row, end_col = map(int, sys.stdin.readline().split())
-    passengers_start[idx] = [start_row - 1, start_col - 1]
-    # board[start_row - 1][start_col - 1] = 2
-    passengers_end[idx] = [end_row - 1, end_col - 1]
-
-def check_shortest_dist():
-
-    dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    # shortest_dist = {idx : sys.maxsize for idx in passenger_list}
-    dist_idx = {}
-    check_cnt = 0
-    
+def find_customer():
     visited = [[0 for _ in range(N)] for _ in range(N)]
-    que = deque([[taxi, 0]])
-    visited[taxi[0]][taxi[1]] = 1
-
+    que = deque([[taxi_row, taxi_col, 0]])
+    min_dist = N * N + 1
+    min_loc = (N + 1, N + 1)
     while que:
-        (now_row, now_col), now_dist = que.popleft()
-
-        for passenger_idx, (start_row, start_col) in passengers_start.items():
-            if now_row == start_row and now_col == start_col:
-                if now_dist not in dist_idx:
-                    dist_idx[now_dist] = [[passenger_idx, [now_row, now_col]]]
-                else:
-                    dist_idx[now_dist].append([passenger_idx, [now_row, now_col]])
-                check_cnt += 1
-                break
-        
-        if check_cnt == len(passenger_list):
-            tmp = sorted(list(dist_idx.items()))[0]
-            dist = tmp[0]
-            idx = sorted(tmp[1], key = lambda x: (x[1][0], x[1][1]))[0][0]
-            return (idx, dist)
-
-        for drow, dcol in dirs:
-            next_row, next_col = now_row + drow, now_col + dcol
-            if 0 <= next_row < N and 0 <= next_col < N and board[next_row][next_col] != 1 and visited[next_row][next_col] == 0:
-                que.append([[next_row, next_col], now_dist + 1])
+        now_row, now_col, now_dist = que.popleft()
+        if board[now_row][now_col] == 2 and now_dist <= min_dist:
+            min_dist = now_dist
+            if now_dist == min_dist:
+                if now_row < min_loc[0] or (now_row == min_loc[0] and now_col < min_loc[1]):
+                    min_loc = [now_row, now_col]
+            else:
+                min_loc = [now_row, now_col]
+            continue
+        for (drow, dcol) in [[-1, 0], [0, -1], [1, 0], [0, 1]]:
+            next_row = now_row + drow
+            next_col = now_col + dcol
+            if 0 <= next_row < N and 0 <= next_col < N and board[next_row][next_col] != 1 and not visited[next_row][next_col]:
                 visited[next_row][next_col] = 1
-    
-    return False, False
+                que.append([next_row, next_col, now_dist + 1])
+    return min_loc, min_dist
 
-def start_end_dist(passenger_idx):
-    dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+def take_customer(s_row, s_col, e_row, e_col):
     visited = [[0 for _ in range(N)] for _ in range(N)]
-    que = deque([[passengers_start[passenger_idx], 0]])
-    visited[passengers_start[passenger_idx][0]][passengers_start[passenger_idx][1]] = 1
-
+    que = deque([[s_row, s_col, 0]])
     while que:
-        (now_row, now_col), now_dist = que.popleft()
-        if [now_row, now_col] == passengers_end[passenger_idx]:
+        now_row, now_col, now_dist = que.popleft()
+        if now_row == e_row and now_col == e_col:
             return now_dist
-        if now_dist > oil:
-            return None
-        
-        for drow, dcol in dirs:
-            next_row, next_col = now_row + drow, now_col + dcol
-            if 0 <= next_row < N and 0 <= next_col < N and not board[next_row][next_col] and not visited[next_row][next_col]:
-                que.append([[next_row, next_col], now_dist + 1])
+        for (drow, dcol) in zip([1, -1, 0, 0], [0, 0, 1, -1]):
+            next_row = now_row + drow
+            next_col = now_col + dcol
+            if 0 <= next_row < N and 0 <= next_col < N and board[next_row][next_col] != 1 and not visited[next_row][next_col]:
                 visited[next_row][next_col] = 1
-    return None
+                que.append([next_row, next_col, now_dist + 1])
+    return -1
 
-passenger_clear = True
-while passenger_list:
-    shortest_passenger_idx, shortest_passenger_dist = check_shortest_dist()
-    if not shortest_passenger_idx:
-        passenger_clear = False
-        # print("********** 갈 수 있는 곳이 없습니다 ************")
-        break
-    # print(f"========== SHORTEST PASSENGER {shortest_passenger_idx} with {shortest_passenger_dist} dist ==========")
-    passenger_list.remove(shortest_passenger_idx)
+def solution():
+    global taxi_oil, taxi_row, taxi_col, board
+    customer_cnt = 0
+    while customer_cnt < M:
+        (customer_row, customer_col), customer_dist = find_customer()
+        if customer_dist == N * N + 1:
+            return  -1
+        end_dist = take_customer(customer_row, customer_col, *customers[(customer_row, customer_col)])
+        if end_dist == -1 or taxi_oil < customer_dist + end_dist:
+            return -1
+        customer_cnt += 1
+        board[customer_row][customer_col] = 0
+        (taxi_row, taxi_col) = customers[(customer_row, customer_col)]
+        taxi_oil = taxi_oil - customer_dist + end_dist
+    return taxi_oil
 
-    board[passengers_start[shortest_passenger_idx][0]][passengers_start[shortest_passenger_idx][1]] = 0
-    taxi = [passengers_start[shortest_passenger_idx][0], passengers_start[shortest_passenger_idx][1]]
-    oil -= shortest_passenger_dist
-    # print("승객에게 도착했습니니다 => oil :", oil)
-    if oil <= 0:
-        passenger_clear = False
-        # print("************ 연료가 소진되었습니다. *************")
-        break
-
-    going_dist = start_end_dist(shortest_passenger_idx)
-    if going_dist == None:
-        passenger_clear = False
-        # print("********* 도착지로 갈 수 없습니다 *********")
-        break
-    taxi = passengers_end[shortest_passenger_idx]
-    del(passengers_start[shortest_passenger_idx])
-    del(passengers_end[shortest_passenger_idx])
-    oil -= going_dist
-    # print(f"도착지까지 {going_dist}만에 도착했습니다. => oil :", oil)
-    if oil < 0:
-        passenger_clear = False
-        # print("************* 도착지에 도착하는 과정에서 연료가 소진되었습니다. ****************")
-        break
-
-    oil += 2 * going_dist
-    # print("무사히 도착하여 연료가 추가되었습니다. => oil :", oil)
-
-print(oil if passenger_clear else -1)
+print(solution())
